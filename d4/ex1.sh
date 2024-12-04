b121f8158b007b5528ceb5fee1c28da2198a8f3f
Valentin Grimaldi <valentin.grimaldi@cacd2.fr>
2024-12-04 09:47:20 +0100

    Add d4 p1

---
+++ d4/ex1.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+export AOC_DAY=4
+source ../lib/utils.sh
+set_LINE_ARRAY
+
+function get_cross_word() {
+  local x y
+
+  x=$1
+  y=$2
+
+  for x_dir in -1 0 1; do
+    (( x_dir == -1 && x < 3 )) && continue
+    (( x_dir == 1 && x + 3 > ${#LINE_ARRAY[0]} - 1 )) && continue
+    for y_dir in -1 0 1; do
+      (( y_dir == 0 && x_dir == 0 )) && continue
+      (( y_dir == -1 && y < 3 )) && continue
+      (( y_dir == 1 && y + 3 > ${#LINE_ARRAY[@]} - 1 )) && continue
+      
+      echo ${LINE_ARRAY[$y]:$x:1}${LINE_ARRAY[$(( y + y_dir ))]:$(( x + x_dir )):1}${LINE_ARRAY[$(( y + y_dir * 2 ))]:$(( x + x_dir * 2 )):1}${LINE_ARRAY[$(( y + y_dir * 3 ))]:$(( x + x_dir * 3 )):1}
+    done
+  done
+}
+
+RES=0
+LEN=${#LINE_ARRAY[0]}
+X_INDEXES=( $(seq 0 $(( LEN - 1 ))) )
+for j in ${!LINE_ARRAY[@]}; do
+  for i in ${X_INDEXES[@]}; do
+    for word in $(get_cross_word $i $j); do
+      [[ $word == "XMAS" ]] && (( ++RES ))
+    done
+  done
+done
+
+echo $RES
